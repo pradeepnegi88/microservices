a06cef21b5418aec8fe2ff27c5b94e2af167a8f8
PradeepNegi <pradeepnegi2012@gmail.com>
2019-11-10 14:22:06 +0530

    Rest Template added to call external apis

--- movie-catalog-service/src/main/java/com/pradeep/moviecatalogservice/resource/MovieCatalogService.java
+++ movie-catalog-service/src/main/java/com/pradeep/moviecatalogservice/resource/MovieCatalogService.java
@@ -1,21 +1,34 @@
 package com.pradeep.moviecatalogservice.resource;
 
 import com.pradeep.moviecatalogservice.model.CatalogItem;
+import com.pradeep.moviecatalogservice.model.Movie;
+import com.pradeep.moviecatalogservice.model.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogService {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalogs(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformer", "description", 4)
+        RestTemplate restTemplate = new RestTemplate();
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movie/" + rating.getMovieId(), Movie.class);
+           return new CatalogItem(movie.getName(), "description", rating.getRating());
+        }).collect(Collectors.toList());
+
     }
 
 }
